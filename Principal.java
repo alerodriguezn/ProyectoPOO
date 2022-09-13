d04639a5cc187a55ac48090b5c3f86131ff957d7
alerodriguezn <navarrocmo@gmail.com>
2022-09-13 11:24:29 -0600

    Creacion de menu profesor y solicitud de datos al usuario en el menu

--- Principal.java
+++ Principal.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
 
 public class Principal {
-
 
     public static void limpiarConsola(){
         System.out.print("\033[H\033[2J");
@@ -16,13 +19,13 @@ public class Principal {
         limpiarConsola();
         System.out.println(" ==========================================");
         System.out.println(" ============ INICIO DE SESION ============\n");
-        System.out.println(" ==== 1. Iniciar sesion como Profesor =====");
-        System.out.println(" ==== 2. Iniciar sesion como Coordinador ==\n");
+        System.out.println(" ==== 1. Iniciar sesion como Coordinador ==");
+        System.out.println(" ==== 2. Iniciar sesion como Profesor =====\n");
         System.out.println(" =========================================="); 
-        
-        
     }
-
+    /**
+     * Funcion que despliega el menu del coordinador
+     */
     public static void menuCoordinador(){
         limpiarConsola();
         System.out.println("--------------------------------------------------");
@@ -40,6 +43,20 @@ public class Principal {
         System.out.println("--------------------------------------------------");
     }
 
+    public static void menuProfesor(){
+        limpiarConsola();
+        System.out.println("--------------------------------------------------");
+        System.out.println("|                Menu Profesor                   |");
+        System.out.println("|    [1]. Registrar Calificaciones               |");
+        System.out.println("|    [2]. Registro de Tiempo Adicional           |");
+        System.out.println("|    [3]. Adecuaciones                           |");
+        System.out.println("|    [4]. Reporte de incidencia significativa    |");
+        System.out.println("|    [5]. Sesiones de tutoria y asistencia       |");
+        System.out.println("|    [S]. Atras                                  |");
+        System.out.println("--------------------------------------------------");
+
+    }
+
     /**
      * Funcion que valida el usuario y la contrasena ingresada por el Usuario
      * @param usuario Nombre de Usuario
@@ -50,7 +67,7 @@ public class Principal {
     public static boolean validarUsuarioContrasena(String usuario, String contrasena, ArrayList<Usuario> listaUsuarios) {
         boolean datosCorrectos = false;
         for (Usuario us : listaUsuarios) {
-            if (us.getUsuario() == usuario && us.getContrasena() == contrasena){
+            if (us.getUsuario().equals(usuario) && us.getContrasena().equals(contrasena)){
                 System.out.println("Datos Correctos");
                 datosCorrectos = true;
                 return datosCorrectos;
@@ -62,11 +79,9 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        // ============================= PRUEBAS ==============================
+        /*  ============================= PRUEBAS ==============================
 
-        
         ArrayList<Estudiante> listaEstudiantes = new ArrayList<Estudiante>();
-
         Estudiante est1 = new Estudiante("Alejandro Rodriguez", 2022230350, new Date(2002,11,19), (byte)19, "Hombre", "El Tanque");
         Estudiante est2 = new Estudiante("Deivid Matute", 2022180126, new Date(2002,11,21), (byte)19, "Hombre", "Santa Clara");
 
@@ -74,6 +89,15 @@ public class Principal {
         listaEstudiantes.add(est2);
 
         // El arraylist permite hacer un arreglo de la cantidad de elementos que quiera
+        
+
+        String usuarioPrueba = "leo";
+        String passwordPrueba = "leo123";
+
+        validarUsuarioContrasena(usuarioPrueba, passwordPrueba, listaUsuarios);
+        menuCoordinador();
+         ============================= PRUEBAS ==============================*/
+
         ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
         
         Usuario us1 = new Usuario("Leo", "8523-1232", "leo@leo.com", "leo", "leo123");
@@ -82,17 +106,43 @@ public class Principal {
         listaUsuarios.add(us1);
         listaUsuarios.add(us2);
 
+        while(true){
+            menuInicioSesion();
 
-        String usuarioPrueba = "leo";
-        String passwordPrueba = "leo123";
+            System.out.print("Elija Una Opcion: ");
+            Scanner in = new Scanner(System.in);
+            String opcionLogin = in.nextLine();
 
-        // ============================= PRUEBAS ==============================
+            if (opcionLogin.equals("1")){
 
-        validarUsuarioContrasena(usuarioPrueba, passwordPrueba, listaUsuarios);
+                System.out.print("Usuario: ");
+                String usuario = in.nextLine();
 
-        menuCoordinador();
+                System.out.print("Contraseña: ");
+                String contrasena = in.nextLine();
 
+                if(validarUsuarioContrasena(usuario, contrasena, listaUsuarios)){
+                    menuCoordinador();
+                    break;// Eliminar esto despues
+                }
+                
+                
+            }else if(opcionLogin.equals("2")){
+                System.out.print("Usuario: ");
+                String usuario = in.nextLine();
 
+                System.out.print("Contraseña: ");
+                String contrasena = in.nextLine();
+
+                if(validarUsuarioContrasena(usuario, contrasena, listaUsuarios)){
+                    menuProfesor();
+                    break;// Eliminar esto despues
+                }
+            }else{
+                System.out.println("Dato incorrecto");
+            }
+
+        }
         
     }
     
